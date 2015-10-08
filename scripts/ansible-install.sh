add9cb96bc077a2f19f7d684b8f1290e3644b318
Yuri Vysotskiy <yuri.vysotskiy@gmail.com>
2015-10-08 14:22:47 +0300

    updated scripts and template for my needs

--- scripts/ansible-install.sh
+++ scripts/ansible-install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -eux
 
+# Add vagrant user to sudoers.
+echo "vagrant        ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers
+sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
+
 # Install Ansible repository.
 apt-get -y update && apt-get -y upgrade
 apt-get -y install software-properties-common
@@ -8,3 +12,5 @@ apt-add-repository ppa:ansible/ansible
 # Install Ansible.
 apt-get -y update
 apt-get -y install ansible
+
+ansible-galaxy install -i geerlingguy.packer-debian geerlingguy.nfs
